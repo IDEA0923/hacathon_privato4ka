b69acd7d142fbb67076b5a7a9cd6a0e280761370
IDEA <jjggsfx@gmail.com>
2026-05-24 19:10:22 +1000

    edited init.sql

--- init.sql
+++ init.sql
@@ -9,7 +9,13 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS  events(
                                 id SERIAL PRIMARY KEY,
                                 name_1 VARCHAR(20),
-                                date_of_event TIMESTAMP DEFAULT NOW() + INTERVAL '3 days',
+                                date_start TIMESTAMP DEFAULT NOW() + INTERVAL '3 days',
+                                date_end TIMESTAMP DEFAULT NOW() + INTERVAL '4 days',
+                                class_start INT DEFAULT 0, 
+                                class_end INT DEFAULT 11, 
+                                lvl VARCHAR(20),    #@level
+                                frm VARCHAR(20),    #@from / source
+                                lnk VARCHAR(40)
                                 subjects VARCHAR(20),
                                 description_1 VARCHAR(500)
                                 )
