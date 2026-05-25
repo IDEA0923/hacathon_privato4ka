2bbe7419b91c36112cf6e7d6573b44a32106f80b
IDEA <jjggsfx@gmail.com>
2026-05-25 22:41:24 +1000

    try hotfix

--- parser_old/sources/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseSource, HttpClient
-from .olimpiada_ru import OlimpiadaRu
-from .rsr_olymp import RsrOlymp
-
-ALL_SOURCES: list[type[BaseSource]] = [OlimpiadaRu, RsrOlymp]
-
-__all__ = ["BaseSource", "HttpClient", "OlimpiadaRu", "RsrOlymp", "ALL_SOURCES"]
