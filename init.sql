ef31526218dbb59f25fd21ffe21aa07dc30c7063
idea <jjggsfx@gmail.com>
2026-05-25 12:54:51 +1000

    fix init.sql

--- init.sql
+++ init.sql
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS  events(
                                 date_end TIMESTAMP DEFAULT NOW() + INTERVAL '4 days',
                                 class_start INT DEFAULT 0, 
                                 class_end INT DEFAULT 11, 
-                                lvl VARCHAR(20),    #@level
-                                frm VARCHAR(20),    #@from / source
+                                lvl VARCHAR(20),
+                                frm VARCHAR(20),
                                 lnk VARCHAR(40)
                                 subjects VARCHAR(20),
                                 description_1 VARCHAR(500)
